88a53d5759389790e34c921213ac91c56077595a
Maksym <maksymsydorovych@gmail.com>
2023-03-31 19:02:38 +0200

    fix bugs with Hotels to db connection

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import authentication from './authentication';
 import users from './users';
 import products from './products';
+import hotels from "./hotels";
 
 const router = express.Router();
 
@@ -10,6 +11,7 @@ export default (): express.Router => {
   authentication(router);
   users(router);
   products(router)
+  hotels(router)
 
   return router;
 };
