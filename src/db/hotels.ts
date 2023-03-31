c5acfb76afa15165c24a023b025e20be58f2955e
Maksym <maksymsydorovych@gmail.com>
2023-03-31 20:06:28 +0200

    token hotels

--- src/db/hotels.ts
+++ src/db/hotels.ts
@@ -15,8 +15,8 @@ export const ProductModel = mongoose.model('Hotel', HotelsSchema);
 
 export const getHotels = () => ProductModel.find();
 export const getHotelByTitle = (title: string) => ProductModel.findOne({ title });
-// export const getProductBySessionToken = (sessionToken: string) => ProductModel.findOne({ 'authentication.sessionToken': sessionToken });
+export const getProductBySessionToken = (sessionToken: string) => ProductModel.findOne({ 'authentication.sessionToken': sessionToken });
 export const getHotelById = (id: string) => ProductModel.findById(id);
 export const createHotel = (values: Record<string, any>) => new ProductModel(values).save().then((product) => product.toObject());
 export const deleteHotelById = (id: string) => ProductModel.findOneAndDelete({ _id: id });
-// export const updateProductById = (id: string, values: Record<string, any>) => ProductModel.findByIdAndUpdate(id, values);
+export const updateProductById = (id: string, values: Record<string, any>) => ProductModel.findByIdAndUpdate(id, values);
